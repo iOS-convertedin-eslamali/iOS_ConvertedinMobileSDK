1abe42031ec130e5d50ebe1416081178237671a9
iOS-convertedin-eslamali <145397952+iOS-convertedin-eslamali@users.noreply.github.com>
2023-09-25 11:14:06 +0300

    first commit

---
+++ ConvertedinMobileSDK/ConvertedinMobileSDK/ConvertedinMobileSDK.h
@@ -0,0 +1,19 @@
+//
+//  ConvertedinMobileSDK.h
+//  ConvertedinMobileSDK
+//
+//  Created by Emmanuel Okwara on 18/09/2020.
+//  Copyright © 2020 Macco. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ConvertedinMobileSDK.
+FOUNDATION_EXPORT double ConvertedinMobileSDKVersionNumber;
+
+//! Project version string for ConvertedinMobileSDK.
+FOUNDATION_EXPORT const unsigned char ConvertedinMobileSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ConvertedinMobileSDK/PublicHeader.h>
+
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# Convertedin Mobile SDK
+This is the a simple framework for convertedin partners
